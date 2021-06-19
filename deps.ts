2f73b71f656ad937825a88b8285528d5a79763d9
SebNae <naesebi2000@gmail.com>
2021-06-19 14:45:38 +0300

    Added deps.ts

---
+++ deps.ts
@@ -0,0 +1,3 @@
+import { serve } from "https://deno.land/std@0.93.0/http/server.ts";
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
